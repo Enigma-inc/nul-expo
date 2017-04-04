ddd253d5117c0b8628f29fa0eb2886d7a0ab6b5c
Mochesane <mpalilabmannkheekhe@gmail.com>
2017-04-04 02:29:13 -0700

    Remove unnecessary partials

--- resources/views/pages/conference/index.blade.php
+++ resources/views/pages/conference/index.blade.php
@@ -1,7 +1,113 @@
 @extends('partials.master')
 
 @section('content')
-    @include('pages.conference.partials.conference')
+    <section class="padding-20">
+        <div  id="app" class="auto-container">
+
+        <div class="sec-title wow fadeInUp text-center" data-wow-delay="200ms" data-wow-duration="1000ms">
+            <h2>Conference Concept</h2>
+            </div>
+            <div class="row clearfix" >
+                        <div class="row wow fadeInUp" data-wow-delay="200ms" data-wow-duration="1500ms">
+                        <img src="{{asset('images/conference/nul-roma.jpg')}}" alt="Concept" class="thumbnail pull-left margin-10 conference-img-lg">
+                            <p class="text-align-just">
+                                The National University of Lesotho (NUL) is located at about 34 km from Maseru, the Capital city of the Kingdom of Lesotho. The origin of the university dates back to 8th April, 1945 when a Catholic University College, the Pius XII College, was founded at Roma by the Roman Catholic Hierarchy of Southern Africa. Since 1945, various major developments in the history of the University have taken place both in terms of physical infrastructure development and growth in students' enrolment. 
+                            </p>
+
+                            <p class="text-align-just">
+                                On 1st January, 1964, the Pius XII College became an integral part of the independent, non-denominational University of Basutoland, Bechuanaland Protectorate, and Swaziland (UBBS), who’s Charter was granted by Queen Elizabeth II. In 1966, the UBBS became the University of Botswana, Lesotho and Swaziland or UBLS, in conformity with the names chosen after independence of the three countries. On 20th October 1975, the National University of Lesotho (NUL) was formally established through an Act of the parliament. It is the sole public university entrusted with the nation building exercise.
+                            </p>
+
+                            <p class="text-align-just">
+                                In the recent global economic meltdown of 2008, Lesotho as a small nation experienced a serious decline in the job market for the graduates. This posed a serious challenge to the University to respond to this ‘national crisis’ as declared by His Majesty King Letsie III, the Head of State, who is also the Chancellor of the University. Following this, the University shifted its research focus from purely basic to applied, innovative and impactful research addressing the economic challenges.
+                            </p>
+
+                            <p class="text-align-just">
+                                It is in this pursuit that the University held two Science and Technology Research and Innovation Expo in 2015 and 2016 showcasing on 60, and 180 projects, respectively, that had potential, for driving entrepreneurship to the community and hence, stimulating economic growth to the thousands of graduates who would otherwise be lost into poverty. These two expos also exposed the need to infuse international views and expertise; and thus, the idea of hosting an international conference originated. 
+                            </p>
+
+                            <p class="text-align-just">
+                                This 2018 National University of Lesotho International Science and Technology Innovation Conference & Expo (NULSTICE-2018) is meant to provide an opportunity to turn ideas into action. It is expected that the attending scholars from all over the world will share their basic research findings, lessons in university-industry cooperation, successes and failures in commercialization and policy recommendations in any of the areas mentioned above. NULSTICE-2018 and the parallel events combined together are going to be bigger in scope and wider in reach. While the previous events were just one day expos and workshops, this 2018 event will combine together an International Conference & Expo, student’s competitions (proposed Vodacom-NUL Hackathon), Gala Dinner and a Daytrip for the international visitors. 
+                            </p>
+                        </div>
+                        
+
+                        <div class="row wow fadeInUp" data-wow-delay="200ms" data-wow-duration="1500ms">
+                            <p class="text-align-left color-primary">
+                                <strong>The Conference</strong>
+                            </p>
+                            <img src="{{asset('images/conference/conf.jpg')}}" alt="Conference" class="thumbnail pull-left margin-10 conference-img">
+                            <p class="text-align-just">
+                                National University of Lesotho International Science, Technology and Innovation Conference & Expo (NULSTICE-2018) will be a convergence of international scholars from many areas of Science and Technology with the theme, “Resource Mobilization and Entrepreneurship Action for Development.” The main areas include Renewable Energy, Biotechnology, Environment and Natural Resources Management, Food and Water Safety, Climate Change Challenges and Mitigation Strategies, Agriculture and Food Security, Material Science and Nano-Technology, Entrepreneurship and Technology, Information and Communication Technology. 
+                            </p>
+
+                            <p class="text-align-just">
+                                It is our collective duty to promote responsible business practices by mobilizing rresources and assisting entrepreneurship actions in each sector to ensure self-employment and job creation amongst the youth to develop the nation. One of the key messages to be conveyed in this conference is that responsibility cannot be outsourced. We have to develop flexible policies that would help in promoting ideas and give confidence and support to those who want to build the future. Taking part in business means to take responsibility to adopt responsible business practices that take into account both the bottom line and the impact of their activities on society. Academia, Governments and Non-government institutions have responsibility to protect fundamental rights of the do and the don’ts to improve and provide a forum for dialogue, peer learning, standard setting, analysis, and best policy recommendations to promote the innovative practices in the nation in particular, and the world in general.
+                            </p>
+
+                            <p class="text-align-just">
+                                We hope that such a joint international conference and expo on Science, Technology and Innovation forum will allow us to take joint actions to assist the youth in our respective countries to bring positive changes in societal development and protect the environment. 
+                            </p>
+                        </div>
+                        
+                        <div class="row wow fadeInUp" data-wow-delay="200ms" data-wow-duration="1500ms">
+                            <p class="text-align-left color-primary">
+                                <strong>The Expo</strong>
+                            </p>
+                                <img src="{{asset('images/conference/expo.jpg')}}" alt="Expo" class="thumbnail pull-left margin-10 conference-img">
+                            <p class="text-align-just">
+                                The 2018 Expo will be the continuation of the first two (held in 2015 and 2016). This event is mainly targeting the local audience and it is meant to help the society appreciate an important role that can be played by science and technology in daily life and to help attract funding and commercialization of academic projects. 
+                            </p>
+
+                            <p class="text-align-just">
+                                For the past two years, the event has generated an amazing amount of interest in the country. While most people appreciate and communicate with the NUL through the media outreach platforms, the Expo is unique in the sense that it provides a means through which members of the society get a tangible feel of the projects. The result has been an intensive networking and in some cases, emergence of new companies. 
+                            </p>
+
+                            <p class="text-align-just">
+                                Traditionally, the main people who exhibit their products and projects in this expo are NUL teachers, students and graduates. However, the following expo will encourage the participation of innovators from all over the country. Nevertheless, it is important to note that only innovative products and projects will be allowed (Indeed our definition of innovation is much more relaxed as it may even refer to products which are new in the context of being made in Lesotho, although they have been made and used elsewhere for centuries).
+                            </p>
+                        </div>
+                        
+                        <div class="row wow fadeInUp" data-wow-delay="200ms" data-wow-duration="1500ms">
+                            <p class="text-align-left color-primary">
+                                <strong>Vodacom-NUL Hackathon </strong>
+                            </p>
+                            <img src="{{asset('images/conference/hackathon.jpg')}}" alt="Hackathon" class="thumbnail pull-left margin-10 conference-img">
+                            <p class="text-align-just">
+                                Since 2014, Vodacom Lesotho Foundation has partnered with the NUL Science and Technology Society, driven by a group of NUL students, to stage what has been known as the ‘Code for Development’ Hackathon, where programmers, designers, entrepreneurs, and problem solvers, come together to compete in solving real world problems, using innovation and technology, over a period of 24 hours.
+                            </p>
+
+                            <p class="text-align-just">
+                                This competition has been won by a number of NUL students and graduates. However, it is open to all whiz kids from around the country and it is a good example of how to challenge and reward technology leaders of tomorrow. NUL is in talks with Vodacom Lesotho Foundation to find out if the event can be part of a large series of events within the NULSTICE-2018 week.
+                            </p>
+                        </div>
+                        
+                        <div class="row wow fadeInUp" data-wow-delay="200ms" data-wow-duration="1500ms">
+                            <p class="text-align-left color-primary">
+                                <strong>The Gala Dinner</strong>
+                            </p>
+                            <img src="{{asset('images/conference/gala-dinner.jpg')}}" alt="Gala-Dinner" class="thumbnail pull-left margin-10 conference-img">
+                            <p class="text-align-just">
+                                The last day of the conference will be marked by a Gala Dinner which will be attended by the NUL Vice Chancellor and dignitaries from the government of Lesotho. The event will give the participants some time to unwind and reflect towards the end of the whole thing. 
+                            </p>
+                        </div>
+                        
+                        <div class="row wow fadeInUp" data-wow-delay="200ms" data-wow-duration="1500ms">
+                            <p class="text-align-left color-primary">
+                                <strong>The Daytrip</strong>
+                            </p>
+                            <img src="{{asset('images/conference/kome.jpg')}}" alt="Kome" class="thumbnail pull-left margin-10 conference-img">
+                            <p class="text-align-just">
+                                Lesotho (The Forgotten Kingdom, as one movie maker would define it) is so small a country; many people around the world just haven’t heard anything about it. The fact that it is “hidden” within South Africa, and completely surrounded by it, does not help either. The result is a country with so many good things that is almost unknown to the outside world.
+                            </p>
+
+                            <p class="text-align-just">
+                                The Daytrip will be a one-day short trip around the country meant to give the international tourists a feel of this tiny Mountain Kingdom. Given the time of the year, it is hoped that the visitors will have an opportunity to enjoy the scenery of the country and the way of life of the Basotho people at a prime time.
+                            </p>
+                        </div>  
+              </div>
+          </div>    
+</section>
 @stop
 
 @section('scripts')
