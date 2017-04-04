28a0ecf3fc8d46c4344abf41cc19d222b7b71721
Mochesane <mpalilabmannkheekhe@gmail.com>
2017-04-03 23:45:11 -0700

    Reshuffle the menu

--- resources/views/pages/abstract/partials/abstract_format.blade.php
+++ resources/views/pages/abstract/partials/abstract_format.blade.php
@@ -3,20 +3,55 @@
             <div class="sec-title wow fadeInUp" data-wow-delay="200ms" data-wow-duration="1000ms">
                 <h2>Abstract Submission Format NULISTICE 2018</h2>
             </div>
-            <div class="row clearfix">
+            <div>
                     <div class="text-align-just">
-                        <p>
-                            <ul>
+                        <p class="text-align-just">
+                            <ul class="list">
                                 <li>Title  (Font 14 , Times New Romans, bold),</li>
-                                <li>Sure name and initials of author and co-authors, followed by their respective institution address  (Font 12 , Times New Romans),</li>
+                                <li>Surname and initials of author and co-authors, followed by their respective institution address  (Font 12 , Times New Romans),</li>
                                 <li>Abstract (Font 12 (Times New Romans) and word count up to 300,</li>
                                 <li>Line space 1.5,</li>
                                 <li>Key words: Five, separated by a semi-colon(;) and first letter upper case,</li>
-                                <li>Corresponding author: (Font 12, Times New Romans)</li>
-                                
+                                <li>Corresponding author: (Font 12, Times New Romans):
+                                    <ul>
+                                        <li>Identify by * </li>
+                                        <li>Email address</li>
+                                        <li>Telephone and Fax</li>
+                                    </ul>
+                                </li>
                             </ul>
                         </p>
                     </div>
             </div>
+            <div class="sec-title wow fadeInUp" data-wow-delay="200ms" data-wow-duration="1000ms">
+                <h3>Article Title; Times New Roman; Size-14; Line Spacing: 1.5 Paragraph Spacing: Above Paragraph-10pt, Below Paragraph-10pt</h3>
+                <h4>Authors’ information with their institution and post box</h4>
+            </div>
+
+            <p class="text-align-just"><strong><i>Abstract</i></strong></p>
+            <hr>
+
+            <p class="text-align-just">
+                An abstract is a brief, comprehensive summary of the contents of the article; it allows readers to survey the contents of an article quickly and, like a title, 
+                it enables persons interested in the document to retrieve it from abstracting and indexing databases. Most scholarly journals require an abstract. 
+                Consult the instructions to authors or web page of the journal to which you plan to submit your article for any journal-specific instructions. 
+                A well-prepared abstract can be the most important single paragraph in an article. Most people have their first contact with an article by seeing just the abstract, 
+                usually in comparison with several other abstracts, as they are doing a literature search.
+            </p>
+
+            <p class="text-align-just"> 
+                Readers frequently decide on the basis of the abstract whether to read the entire article. The abstract needs to be dense with information. By embedding key words in your abstract, 
+                you enhance the user’s ability to find it. Do not exceed the abstract word limit of the journal to which you are submitting your article. Word limits vary from journal to journal and 
+                typically range from 250 to 300 words. For information on how abstracts are used to retrieve articles, consult Record Structure for APA Databases or any International Standard journals for information.
+            </p>
+            <hr>
+
+            <p class="text-align-just">
+                <strong>Keywords: </strong>
+                Upper case; Semi-colon; Paper template; Abstract; Keywords; Introduction
+                Corresponding author: Identify by *
+                Email address;
+                Telephone and Fax
+            </p>
         </div>
 </section>
