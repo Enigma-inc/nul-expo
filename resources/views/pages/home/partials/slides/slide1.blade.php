130deb8699a6812ad4db7d4bd3bb10de896cd812
Neo Mokoena <mokoena.n.a@gmail.com>
2017-04-04 14:36:12 +0200

    Fix Fees page and Extract Slider partials

---
+++ resources/views/pages/home/partials/slides/slide1.blade.php
@@ -0,0 +1,18 @@
+<li data-transition="fade" data-slotamount="1" data-masterspeed="1000" data-thumb="images/main-slider/image-1.jpg" data-saveperformance="off"
+    data-title="Your Lead Power">
+    <img src="images/main-slider/image-1.jpg" alt="" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat">
+
+    <div class="tp-caption lfb tp-resizeme" 
+    data-x="left" 
+    data-y="center" 
+    data-voffset="-130" 
+    data-speed="1500"
+        data-start="500" data-easing="easeOutExpo" data-splitin="none" data-splitout="none" data-elementdelay="0.01" data-endelementdelay="0.3"
+        data-endspeed="1200" data-endeasing="Power4.easeIn" style="z-index: 4; max-width: auto; max-height: auto; white-space: nowrap;">
+        <div class="big-title"><br/>
+            <h2>National University of Lesotho Science and Technology <br> Innovation Conference and Expo,
+                <span class="slide-date">NULSTICE-2018 </span>
+            </h2>
+        </div>
+    </div>
+</li>
---
+++ resources/views/pages/home/partials/slides/slide3.blade.php
@@ -0,0 +1,32 @@
+ <li data-transition="fade" data-slotamount="1" data-masterspeed="1000" data-thumb="images/main-slider/image-1.jpg"  data-saveperformance="off"  data-title="Your Lead Power">
+                    <img src="images/main-slider/image-1.jpg"  alt=""  data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat"> 
+                    
+                    <div class="tp-caption lfb tp-resizeme"
+                    data-x="left" data-hoffset="15"
+                    data-y="center" data-voffset="-100"
+                    data-speed="1500"
+                    data-start="500"
+                    data-easing="easeOutExpo"
+                    data-splitin="none"
+                    data-splitout="none"
+                    data-elementdelay="0.01"
+                    data-endelementdelay="0.3"
+                    data-endspeed="1200"
+                    data-endeasing="Power4.easeIn"
+                    style="z-index: 4; max-width: auto; max-height: auto; white-space: nowrap;"><div class="big-title"><h2>We plan, manage and create <br>the perfect event for you!</h2></div></div>
+                    
+                    <div class="tp-caption lfb tp-resizeme"
+                    data-x="left" data-hoffset="15"
+                    data-y="center" data-voffset="160"
+                    data-speed="1500"
+                    data-start="1500"
+                    data-easing="easeOutExpo"
+                    data-splitin="none"
+                    data-splitout="none"
+                    data-elementdelay="0.01"
+                    data-endelementdelay="0.3"
+                    data-endspeed="1200"
+                    data-endeasing="Power4.easeIn"
+                    style="z-index: 4; max-width: auto; max-height: auto; white-space: nowrap;"><div class="links"><a href="#" class="default-btn orange hvr-bounce-to-right">Plan an event</a> <a href="#" class="default-btn blue hvr-bounce-to-right">7-DAYS FREE TRIAL</a></div></div>
+                    
+                    </li>
