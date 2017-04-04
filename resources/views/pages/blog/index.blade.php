dd2baa4beba9ad6684f94bcb01b4aa5c2ab0cef1
Mochesane <mpalilabmannkheekhe@gmail.com>
2017-04-04 02:33:38 -0700

    Modify index files

--- resources/views/pages/blog/index.blade.php
+++ resources/views/pages/blog/index.blade.php
@@ -1,7 +1,27 @@
 @extends('partials.master')
 
 @section('content')
-  @include('pages.blog.partials.blog')
+  <section >
+    <div  id="app" class="auto-container">
+
+        <div class="sec-title wow fadeInUp text-center" data-wow-delay="200ms" data-wow-duration="1000ms">
+            <h2>Our Blog</h2>
+            <h6> Source: <a href="http://www.nulresearchandinnovations.co.ls/blog" target="_blank" style="color: #e13f30"><i>NUL Research & Innovations</i></a></h6>
+        </div>
+       <!-- <div class="sec-text wow fadeInUp" data-wow-delay="200ms" data-wow-duration="1000ms">
+            <p>For a full list of prices please take a look at our table of pricing plans.</p>
+        </div>-->
+
+
+        <div class="row clearfix">
+
+            <!--Table Column-->
+            <blog></blog>
+
+        </div>
+
+    </div>
+</section>
 @stop
 
 @section('scripts')
