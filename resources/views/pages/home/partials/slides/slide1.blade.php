293a73ae0722c76db5a8b2ced8e6e8ba41312e7b
Neo Mokoena <mokoena.n.a@gmail.com>
2017-04-05 12:26:48 +0200

    Add Responsivenes on Slide and Fees

--- resources/views/pages/home/partials/slides/slide1.blade.php
+++ resources/views/pages/home/partials/slides/slide1.blade.php
@@ -4,7 +4,7 @@
 
     <div class="tp-caption lfb tp-resizeme" data-x="left" data-y="center" data-voffset="-130" data-speed="1500" data-start="500"
         data-easing="easeOutExpo" data-splitin="none" data-splitout="none" data-elementdelay="0.01" data-endelementdelay="0.3"
-        data-endspeed="1200" data-endeasing="Power4.easeIn" style="z-index: 4; max-width: auto; max-height: auto; white-space: nowrap;">
+        data-endspeed="1200" data-endeasing="Power4.easeIn" style="z-index: 4;left:20%; max-width: auto; max-height: auto; white-space: nowrap;">
         <div class="big-title"><br/>
             <h2>National University of Lesotho Science and Technology <br> Innovation Conference and Expo
             </h2>
