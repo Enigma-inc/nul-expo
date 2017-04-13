79d6786d114904466a1f68c060c838fa36af27ff
Neo Mokoena <mokoena.n.a@gmail.com>
2017-04-13 15:28:51 +0200

    Add RERIS 2018

--- resources/views/pages/home/partials/slides/slide1.blade.php
+++ resources/views/pages/home/partials/slides/slide1.blade.php
@@ -4,11 +4,14 @@
     <div class="tp-caption lfb tp-resizeme" data-x="left" data-y="center" data-voffset="-130" data-speed="1500" data-start="500" data-easing="easeOutExpo" data-splitin="none" data-splitout="none" data-elementdelay="0.01" data-endelementdelay="0.3" data-endspeed="1200"
         data-endeasing="Power4.easeIn" style="z-index: 4;left:20%; max-width: auto; max-height: auto; white-space: nowrap;">
         <div class="big-title"><br/>
-            <h2>National University of Lesotho Science and Technology <br> Innovation Conference and Expo
+            <h2>
+            National University of Lesotho International Science and Technology <br> Innovation Conference and Expo  <br/>
+            <span class="with-lg">with</span> <br>
+            Renewable Energy Research and Innovation Symposium
             </h2>
             <div class="divider"></div>
-            <span id="nulistice"> 
-                    
+            <span > 
+                    NULISTICE 2018 <span class="with-sm"> with</span> RERIS 2018
                    
                 </span>
             <br> <small class="slide-date"><i class="fa fa-calendar"></i> 23 - 26 January 2018</small>
