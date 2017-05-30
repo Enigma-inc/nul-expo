d87d08bb5358f58bc5b462ac4696f535c24ea8ae
Neo Mokoena <mokoena.n.a@gmail.com>
2017-05-30 17:04:04 +0200

    Update Fees Table

---
+++ resources/views/pages/fees/fees-list.blade.php
@@ -0,0 +1,30 @@
+<section class="price-plans">
+    <div class="auto-container">
+
+        <div class="sec-title wow fadeInUp" data-wow-delay="200ms" data-wow-duration="1000ms">
+            <h2 class="header">Fee structure for conference registration</h2>
+        </div>
+        <div class="fees-container">
+           <div class="fees-header">Category</div>
+           <div class="fees-header">Regular Amounts</div>
+           <div class="fees-header">Student Amounts</div>
+        </div>
+         <div class="fees-container">
+           <div class="fees-body">International </div>
+           <div class="fees-body">USD <span>450</span></div>
+           <div class="fees-body">USD <span>200</span></div>
+        </div>
+         <div class="fees-container">
+           <div class="fees-body">Regional </div>
+           <div class="fees-body">USD <span>325</span></div>
+           <div class="fees-body">USD <span>200</span></div>
+        </div>
+
+         <div class="col-xs-12">
+           <p>For more information <a href="{{route('contact.create')}}">contact us</a></p>
+             
+        </div>
+        </div>
+
+    </div>
+</section>
