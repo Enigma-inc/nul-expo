c1ff13d8392f177b384adf7818dc7d06b5f46676
Neo Mokoena <mokoena.n.a@gmail.com>
2017-09-07 08:16:32 +0200

    Back to old look

--- resources/views/pages/home/partials/slides/slide1.blade.php
+++ resources/views/pages/home/partials/slides/slide1.blade.php
@@ -1,5 +1,5 @@
 <li data-transition="fade" data-slotamount="1" data-masterspeed="1000" data-thumb="images/main-slider/image-1.jpg" data-saveperformance="off" data-title="">
-    <img src="{{asset('images/slider/'.$slide)}}" alt="" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat">
+
 
     <div class="tp-caption lfb tp-resizeme" data-x="left" data-y="center" data-voffset="-130" data-speed="1500" data-start="500" data-easing="easeOutExpo" data-splitin="none" data-splitout="none" data-elementdelay="0.01" data-endelementdelay="0.3" data-endspeed="1200"
         data-endeasing="Power4.easeIn" style="z-index: 4;left:20%; max-width: auto; max-height: auto; white-space: nowrap;">
@@ -10,9 +10,9 @@
             Africa-EU Renewable Energy Research and Innovation Symposium
             </h2>
             <div class="divider"></div>
-            <span > 
+            <span >
                     NULISTICE 2018 <span class="with-sm"> with</span> RERIS 2018
-                   
+
                 </span>
             <br> <small class="slide-date"><i class="fa fa-calendar"></i> 23 - 26 January 2018</small>
 
