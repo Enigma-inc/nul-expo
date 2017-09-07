aebf7c7430d8212ac50d232e60e7dc652f7dcfff
Neo Mokoena <mokoena.n.a@gmail.com>
2017-09-07 08:28:15 +0200

    Revert Look

--- resources/views/pages/home/partials/slides/slide1.blade.php
+++ resources/views/pages/home/partials/slides/slide1.blade.php
@@ -1,5 +1,5 @@
 <li data-transition="fade" data-slotamount="1" data-masterspeed="1000" data-thumb="images/main-slider/image-1.jpg" data-saveperformance="off" data-title="">
-
+    <img src="{{asset('images/slider/'.$slide)}}" alt="" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat">
 
     <div class="tp-caption lfb tp-resizeme" data-x="left" data-y="center" data-voffset="-130" data-speed="1500" data-start="500" data-easing="easeOutExpo" data-splitin="none" data-splitout="none" data-elementdelay="0.01" data-endelementdelay="0.3" data-endspeed="1200"
         data-endeasing="Power4.easeIn" style="z-index: 4;left:20%; max-width: auto; max-height: auto; white-space: nowrap;">
