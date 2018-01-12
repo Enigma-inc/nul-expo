0ceed8e456b4a67ef65b181fa7ec321218e69945
Neo Mokoena <mokoena.n.a@gmail.com>
2018-01-12 11:41:46 +0200

    Deploy Metroplitan Logo

--- resources/assets/js/components/live/nulistice.vue
+++ resources/assets/js/components/live/nulistice.vue
@@ -1,48 +1,53 @@
 <template>
 <v-flex class="event" v-if="event">
-   <v-card color="blue-grey darken-2" class="white--text event-card" >
+     <v-card color="blue-grey darken-2" class="white--text event-card" hover>
             <v-card-title primary-title>
               <div class="headline">
-                  <div class="title">{{event.title}}</div>
+                  <div class="title ">{{event.title}}</div>
                   <div class="details">
-                      <div class="time">09:00</div>
-                      <div class="room">MACHACHE</div>
+                      <div class="time">
+                          <v-icon left>access_time</v-icon>
+                          <span class="color-dark">09:00</span>
+                     </div>
+                      <div class="room">
+                          <v-icon left>home</v-icon>
+                        <span class="color-dark">MACHACHE</span>
+                      </div>
                   </div>
               </div>
-              <div id="nulistice" class="body" v-bar="{
-                  preventParentScroll:false,
-                  scrollThrottle:30
-              }" >
-                   <div class="scrollable">
-                       <p> <b>Chair:</b> Dr Mosotho George, Head, Department of Chemistry
-                    and Chemical Technology, National University of Lesotho</p>
-                    <p> <b>Presentations:</b>
-                    Spatio-Temporal Climate Change Risk Assessment: Mangaung
-                    Metropolitan Municipality, South Africa – Mr Bernard
-                    Moeketsi, University of the Free State, Disaster Management
-                    Training and Education Centre for Africa, South Africa
-                    Performance Evaluation of Grid-Connected Solar PV in the
-                    Tropical Conditions - a Case Study of Karume Institute of
-                    Science and Technology, Zanzibar – Mr Mohammed Haji,
-                    The Nelson Mandela African Institution of Science and
-                    Technology, Tanzania</p>
-                    <p>
-                          Evaluation of Animal Manure as a Feedstock for the Production
-                    of Biogas in comparison with Sewage Water
-                    Microalgae – Ms Tebello Matlosa, Department of Biology
-                    and Biotechnology, Faculty of Science and Technology,
-                    National University of Lesotho
-                    The VIP Latrine in Lesotho: Addressing Technical Flaws
-                    from the Qachas’ Nek District Experience – Prof. Aiyuk
-                    Sunny, Department of Environmental Health, Faculty of
-                    Health Sciences, National University of Lesotho
-                    </p>
-                   </div>
-              </div>
             </v-card-title>
-            <v-card-actions>
-              <v-btn flat dark>View Full Program</v-btn>
-            </v-card-actions>
+            <v-card-text>
+                <div class="body">
+                 <div class="chair">
+                        <div class="flag">
+                            <img :src="'https://restcountries.eu/data/zaf.svg'" />
+                        </div>
+                        <div class="details">
+                            <h4 class="title color-dark">Chair</h4>
+                            <h4 class="name">Dr Mosotho George, Head, Department of Chemistry
+                                and Chemical Technology, National University of Lesotho</h4>
+                        </div>
+                    </div>
+                <v-divider></v-divider>
+                 <div class="presentation">
+                        <div class="flag">
+                            <img :src="'https://restcountries.eu/data/usa.svg'" />
+                        </div>
+                        <div class="details">
+                            <h4 class="title color-dark">Presentation</h4>
+                            <h3 class="presentation-title">Evaluation of Animal Manure as a Feedstock for the Production
+                                of Biogas in comparison with Sewage Water
+                                Microalgae</h3>
+                            <div class="presenter ">
+                                <span class="color-dark">&mdash;</span>
+                                <p class="color-dark">Dr Mosotho George, Head, Department of Chemistry
+                                and Chemical Technology, National University of Lesotho</p>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+
+            </v-card-text>
     </v-card>
  </v-flex>
 </template>
