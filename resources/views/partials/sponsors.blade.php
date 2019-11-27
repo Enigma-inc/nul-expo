6caecedaf2456f1716e7e2a6a904599ccd113cb5
Mochesane Mpali <planetlimar@gmail.com>
2019-11-27 08:53:26 +0200

    Add new style for sponsor images

--- resources/views/partials/sponsors.blade.php
+++ resources/views/partials/sponsors.blade.php
@@ -3,25 +3,23 @@
 </div>
 
 <section class="sponsors profile-sponsors">
-  <div class="center">
-    <a class="col-xs-3 col-sm-2 col-md-1 margin-bottom-10 margin-0" href="http://www.nul.ls" target="_blank"><img src="/images/sponsors/national-university.jpg" alt="" title=""></a>
+    <a href="http://www.nul.ls" target="_blank"><img src="/images/sponsors/national-university.jpg" alt="" title="" class="sponsor-img" style="margin-bottom: 10px;"></a>
     {{-- <a class="col-xs-3 col-sm-2 col-md-1 margin-bottom-10 margin-0" href="https://www.africa-eu-renewables.org" target="_blank"><img src="/images/sponsors/recp.jpg" alt="" title=""></a> --}}
     {{-- <a class="col-xs-3 col-sm-2 col-md-1 margin-bottom-10 margin-0" href="https://www.giz.de/en/html/index.html" target="_blank"><img src="/images/sponsors/giz.jpg" alt="" title=""></a> --}}
     {{-- <a class="col-xs-3 col-sm-2 col-md-1 margin-bottom-10 margin-0" href="https://energypedia.info/wiki/Main_Page" target="_blank"><img src="/images/sponsors/energypedia.jpg" alt="" title=""></a> --}}
     {{-- <a class="col-xs-3 col-sm-2 col-md-1 margin-bottom-10 margin-0" href="http://www.unipid.fi" target="_blank"><img src="/images/sponsors/inipid.jpg" alt="" title=""></a> --}}
     {{-- <a class="col-xs-3 col-sm-2 col-md-1 margin-bottom-10 margin-0" href="http://www.microenergy-international.com" target="_blank"><img src="/images/sponsors/micro-energy.jpg" alt="" title=""></a> --}}
-    <a class="col-xs-3 col-sm-2 col-md-1 margin-bottom-10 margin-0" href="http://www.enigma.co.ls" target="_blank"><img src="/images/sponsors/enigma.jpg" alt="" title=""></a>
+    <a href="http://www.enigma.co.ls" target="_blank"><img src="/images/sponsors/enigma.jpg" alt="" title="" class="sponsor-img" style="margin-bottom: 10px;"></a>
     {{-- <a class="col-xs-3 col-sm-2 col-md-1 margin-bottom-10 margin-0" href="#" target="_blank"><img src="/images/sponsors/sacree.jpg" alt="" title=""></a> --}}
     {{-- <a class="col-xs-3 col-sm-2 col-md-1 margin-bottom-10 margin-0" href="http://lcedn.com/" target="_blank"><img src="/images/sponsors/lcedn.jpg" alt="" title=""></a> --}}
     {{-- <a class="col-xs-3 col-sm-2 col-md-1 margin-bottom-10 margin-0" href="http://www.boliba.co.ls/index.php" target="_blank"><img src="/images/sponsors/boliba.jpg" alt="Boliba" title=""></a> --}}
     {{-- <a class="col-xs-3 col-sm-2 col-md-1 margin-bottom-10 margin-0" href="http://www.etl.co.ls" target="_blank"><img src="/images/sponsors/econet.jpg" alt="Econet" title=""></a> --}}
-    <a class="col-xs-3 col-sm-2 col-md-1 margin-bottom-10 margin-0" href="http://www.pioneer.co.ls" target="_blank"><img src="/images/sponsors/pioneer.jpg" alt="Pioneer Mall" title=""></a>
+    <a href="http://www.pioneer.co.ls" target="_blank"><img src="/images/sponsors/pioneer.jpg" alt="Pioneer Mall" title="" class="sponsor-img" style="margin-bottom: 10px;"></a>
     {{-- <a class="col-xs-3 col-sm-2 col-md-1 margin-bottom-10 margin-0" href="http://coderina.org/" target="_blank"><img src="/images/sponsors/coderina.jpg" alt="Coder In Africa" title=""></a> --}}
-    <a class="col-xs-3 col-sm-2 col-md-1 margin-bottom-10 margin-0" href="https://www.gov.ls/" target="_blank"><img src="/images/sponsors/coat-of-arms.jpg" alt="Government of Lesotho" title=""></a>
-    <a class="col-xs-3 col-sm-2 col-md-1 margin-bottom-10 margin-0" href="https://www.vodacom.co.ls/" target="_blank"><img src="/images/sponsors/vodacom.jpeg" alt="Vodacom" title=""></a>                                         
-    <a class="col-xs-3 col-sm-2 col-md-1 margin-bottom-10 margin-0" href="https://www.thepost.co.ls/" target="_blank"><img src="/images/sponsors/the-post.jpg" alt="The Post" title=""></a>                                         
-    <a class="col-xs-3 col-sm-2 col-md-1 margin-bottom-10 margin-0" href="https://www.avanihotels.com/en/maseru" target="_blank"><img src="/images/sponsors/avani.jpeg" alt="Avani" title=""></a>                                         
-    <a class="col-xs-3 col-sm-2 col-md-1 margin-bottom-10 margin-0" href="https://www.ls.undp.org/" target="_blank"><img src="/images/sponsors/undp.jpeg" alt="UNDP" title=""></a>                                         
-    <a class="col-xs-3 col-sm-2 col-md-1 margin-bottom-10 margin-0" href="http://www.metropolitan.co.ls/" target="_blank"><img src="/images/sponsors/metropolitan.jpg" alt="Metropolitan" title=""></a> 
-  </div>                                                
+    <a href="https://www.gov.ls/" target="_blank"><img src="/images/sponsors/coat-of-arms.jpg" alt="Government of Lesotho" title="" class="sponsor-img" style="margin-bottom: 10px;"></a>
+    <a href="https://www.vodacom.co.ls/" target="_blank"><img src="/images/sponsors/vodacom.jpeg" alt="Vodacom" title="" class="sponsor-img" style="margin-bottom: 10px;"></a>                                         
+    <a href="https://www.thepost.co.ls/" target="_blank"><img src="/images/sponsors/the-post.jpg" alt="The Post" title="" class="sponsor-img" style="margin-bottom: 10px;"></a>                                         
+    <a href="https://www.avanihotels.com/en/maseru" target="_blank"><img src="/images/sponsors/avani.jpeg" alt="Avani" title="" class="sponsor-img" style="margin-bottom: 10px;"></a>                                         
+    <a class="margin-bottom-10 margin-0" href="https://www.ls.undp.org/" target="_blank"><img src="/images/sponsors/undp.jpeg" alt="UNDP" title="" class="sponsor-img" style="margin-bottom: 10px;"></a>                                         
+    <a href="http://www.metropolitan.co.ls/" target="_blank"><img src="/images/sponsors/metropolitan.jpg" alt="Metropolitan" title="" class="sponsor-img" style="margin-bottom: 10px;"></a> 
 </section>
