809d635d6eac0c61b583104309861b8d6fd98cee
Mochesane Mpali <planetlimar@gmail.com>
2019-12-05 09:53:09 +0200

    Footer copyright

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -7,7 +7,7 @@
         	<div class="auto-container">
 
             	<div class="row clearfix ">
-                    <div class="col-md-12 col-sm-12 col-xs-12 footer-logo" style="text-align: center">© Copyright 2016,NUL Research and Innovations  | All rights reserved</div>
+                    <div class="col-md-12 col-sm-12 col-xs-12 footer-logo" style="text-align: center">© Copyright 2019,NUL Research and Innovations  | All rights reserved</div>
                 </div>
             </div>
         </div>
