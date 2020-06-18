d65a503a9869406156c47fad5c1a982c9e355083
Mochesane Mpali <planetlimar@gmail.com>
2020-06-18 12:35:30 +0200

    Add api response

--- app/Http/Controllers/API/BlogAPIController.php
+++ app/Http/Controllers/API/BlogAPIController.php
@@ -3,19 +3,20 @@
 namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
-use Response;
+// use GuzzleHttp\Psr7\Response;
+// use Response;
 use App\Http\Controllers\Controller;
 
 class BlogAPIController extends Controller
 {
     public function index(Request $request){
         $client = new \GuzzleHttp\Client();
-        $response = $client->request('GET', 'https://www.nulresearchandinnovations.co.ls/wp-json/wp/v2/posts?_embed');
+        $responseData = $client->request('GET', 'https://www.nulresearchandinnovations.co.ls/wp-json/wp/v2/posts?_embed');
 
-        if(empty($response)){
-            return $response->getStatusCode('Failed to fetch articles');
+        if(empty($responseData)){
+            return response()->json('Failed to fetch articles', 500);
         }
 
-        return $response->getBody('Article retrieved successfully');
+        return response($responseData->getBody(), 200);
     }
 }
